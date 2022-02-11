35231d9224016fd4366bd388636a8cc29b1c9a10
Daniel De Luna Rodríguez <deluna.rodan21@triana.salesianos.edu>
2022-02-11 12:56:06 +0100

    First commit

---
+++ src/main/java/com/salesianos/dam/DamApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianos/dam/DamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
