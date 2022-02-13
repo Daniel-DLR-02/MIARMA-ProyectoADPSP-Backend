3f74f25ee723a5fdd0f47fb7878d05f4a3b97596
Daniel-DLR-02 <danidlr02@gmail.com>
2022-02-13 23:33:08 +0100

    Arreglado error de properties

--- src/main/java/com/salesianos/dam/DamApplication.java
+++ src/main/java/com/salesianos/dam/DamApplication.java
@@ -1,8 +1,11 @@
 package com.salesianos.dam;
 
+import com.salesianos.dam.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
 public class DamApplication {
 
